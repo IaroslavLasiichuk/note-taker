23efc70bcba6fb699a5c5d2d4a93a1ac620cc8cd
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-03-08 13:40:24 -0700

    Sets up to handle data parsing

--- server.js
+++ server.js
@@ -4,24 +4,30 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const PORT = process.env.PORT || 8080;
+const data = require('./Develop/db/db.json');
 
 // Static middleware pointing to the public folder
-app.use(express.static('public'));
+app.use(express.static('develop/public'));
+app.use('/css', express.static('public/assets/css'))
+app.use('/js', express.static('public/assets/js'))
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Create Express.js routes
+// Landing page with a link to a notes page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
   });
-  
+// Page with existing notes listed 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
 });
-  
-fs.appendFile('Develop/db/db.json', 'Something', () => {
-  console.log('Works');
-})
 
-// listen() method is responsible for listening for incoming connections on the specified port 
+app.get('/api/notes', (req, res) => res.json(data));
+
+// Listen() method is responsible for listening for incoming connections on the specified port 
 app.listen(PORT, ()=> {
     console.log(`Listening at http://localhost:${PORT}` );
 })
